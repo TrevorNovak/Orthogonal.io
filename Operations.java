c403f12ab85d9db034a26629665366063905de41
TrevorNovak <trevornovak.developer@gmail.com>
2016-08-18 10:10:59 -0400

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,20 @@
+package orthonormal;
+
+public interface Operations<T>
+{
+	public T add(T a);
+	
+	public T subtract(T a);
+	
+	public T multiply(T a);
+	
+	public T transpose();
+	
+	public T inverse(T a);
+	
+	public double dotProduct(T a);
+	
+	public T crossProduct(T a);
+	
+	
+}
